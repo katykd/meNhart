4b4487080e26c554fd1e950441ab891d3501d154
katykd <katydenning@gmail.com>
2015-12-13 18:23:27 -0500

    making it centered

--- foodica/js/scripts.js
+++ foodica/js/scripts.js
@@ -1,7 +1,45 @@
-$('.primaryslider').slick({
-    arrows: true,
-    fade: false,
-    dots: true,
-    speed: 1300,
-    autoplay: true
-  });
+var $primarySlider = $('.primaryslider');
+
+// $primarySlider.slick({
+//     arrows: true,
+//     fade: false,
+//     dots: true,
+//     speed: 1300,
+//     autoplay: true
+// });
+
+// $('h1').css({
+//   background: 'red'
+// });
+
+var car = {
+  wheels: 4,
+  radio: true,
+  name: 'Pat',
+  numberOfSeats: 4,
+  sayHello: function(name) {
+    return 'hello ' + name;
+  }
+}
+
+
+/*
+ * Let's make a slider
+ * All the slides on the page are visible
+ * Let's hide all the slides
+ * Then let's get a specific slide, and show that slide
+ */
+
+$('.primaryslider > .3').css({'display': 'block'});
+
+var number = 0;
+setInterval(function () {
+  number = number + 1;
+  $('.primaryslider > div').css({'display': 'none'});
+  $('.primaryslider > .' + number).fadeIn('800');
+  if (number === 3) {
+    number = 0;
+  }
+}, 3000);
+
+console.log($primarySlider);
---
+++ traffic-light/scripts.js
@@ -0,0 +1,34 @@
+/*
+ * For this asignment, create a page with three traffic lights.
+ * When you click on a light, the background of the page should
+ * change to be the same color as the light.
+ * The font colors should also change to make it readable and pretty.
+ */
+console.log('Hey YO');
+
+$('.one').on('click', function(){
+  $('body').css({
+    background: 'red'
+  });
+  $('h1').css({
+    color: 'white'
+  });
+});
+
+$('.two').on('click', function(){
+  $('body').css({
+    background: 'black'
+  });
+  $('h1').css({
+    color: 'white'
+  });
+});
+
+$('.three').on('click', function(){
+  $('body').css({
+    background: 'blue'
+  });
+  $('h1').css({
+    color: 'white'
+  });
+});
